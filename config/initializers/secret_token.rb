e4caf25397b7ee34b2cf9c1b6afbe2c79906a4f3
Ash J <shadowone@linuxmail.org>
2013-12-14 00:13:22 -0700

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'db08c1e08fe7b82a51d4c6cff6fa8629806916f5cc5721dc5eebd8e40a5f87ce1da2f81ab5936ecc634a3fd823c1097f9b46c1b6b491d3efac8241fcb1113695'
